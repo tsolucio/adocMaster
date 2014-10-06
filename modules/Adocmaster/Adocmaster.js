a19a4b03a9f8ed6ed7f2ab9aaaf74b20ac0f7dc8
Joe Bordes <joe@tsolucio.com>
2014-10-06 10:19:40 +0200

    update licenses again due to added space that made diff report false positive change on all files

--- modules/Adocmaster/Adocmaster.js
+++ modules/Adocmaster/Adocmaster.js
@@ -1,20 +1,19 @@
 /*+**********************************************************************************
-- * The contents of this file are subject to the vtiger CRM Public License Version 1.0
-- * ("License"); You may not use this file except in compliance with the License
-- * The Original Code is:  vtiger CRM Open Source
-- * The Initial Developer of the Original Code is vtiger.
-- * Portions created by vtiger are Copyright (C) vtiger.
-- * All Rights Reserved.
-- ************************************************************************************/
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
 function AdocmastersetValueFromCapture(recordid,value,target_fieldname) {
 	//alert(target_fieldname);
-        if(target_fieldname=='adoc_product2'){
-            document.getElementById('adoc_product2').value = recordid;
-				document.getElementById('adoc_product2_display').value = value;}
-                            if(target_fieldname=='adoc_product'){
-                                document.getElementById('adoc_product').value = recordid;
-                                document.getElementById('adoc_product_display').value = value;
-                            }
-		
-
+	if(target_fieldname=='adoc_product2') {
+		document.getElementById('adoc_product2').value = recordid;
+		document.getElementById('adoc_product2_display').value = value;}
+		if(target_fieldname=='adoc_product') {
+			document.getElementById('adoc_product').value = recordid;
+			document.getElementById('adoc_product_display').value = value;
+		}
 }
